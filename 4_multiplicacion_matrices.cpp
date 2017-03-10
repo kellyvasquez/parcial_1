36a0115b3f254196c232428d31f989648f83bdc9
kellyvasquez <kefervaza.19@hotmail.com>
2017-03-10 01:09:45 -0500

    4. multiplicacion de matrices terminado

--- 4_multiplicacion_matrices.cpp
+++ 4_multiplicacion_matrices.cpp
@@ -1,35 +1,70 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-//MATRIZ DINAMICO
+void dato(int fila,int columna,int **matriz);
+void imprimir(int fila,int columna,int **matriz);
+void mult(int f,int c1,int **matri2,int **d,int **resul);
 
-void pedirdato();
-void imprimir();
+void dato(int fila,int columna,int **matriz){
+    for(int i=0;i<fila;i++){
+        for(int j=0;j<columna;j++){
+            printf("(%d,%d): ",i,j);
+            scanf("%d",&matriz[i][j]); }
+    }
+ }
 
-int fila, colum, **p_matriz;
+void imprimir(int fila,int columna,int **matriz){
+    for(int i=0;i<fila;i++){
+        for(int j=0;j<columna;j++){
+            printf("%d\t",*(*(matriz+i)+j)); }
+        printf("\n"); }
+ }
 
-int main (){
-
-    dato();
-    imprimir ();
-    free (p_matriz);
-}
-
-void dato() {
-
-    printf ("Ingrese longitud de filas\n");
-    scanf("%d", &fila);
-    printf ("Ingrese longitud de columnas\n");
-    scanf("%d", &colum);
-
-    p_matriz=(int **)malloc(filas*sizeof(int)*);
-    for (int i=0; i<longcolum; i++){
-        p_matriz[i]=(int*)malloc(colum*sizeof(int)); }
-}
-
-    for (int i=0; i<fila; i++){
-        for (int k=0; k<colum; k++){
-            printf("ingrese dato [%d,%d]\n", flia, colum);
-            scanf("%d", &(*(*(p_matriz+k)+i)));
+void mult (int f,int c,int c1,int **matri2,int **d,int **resul){
+    for(int i=0;i<f;i++){
+        for(int j=0;j<c1;j++){
+            for(int k=0;k<c;k++){
+                resul[i][j]=resul[i][j]+matri2[i][k]*d[k][j]; }
         }
     }
+ }
+
+int main(){
+
+   int columna, fila, f, c, f1, c1;
+   int *matriz, **matri2, **resul, *d, **g;
+
+            printf("*****MULTIPLICACION DE MATRICES DINAMICAS*****\n\n");
+            printf("Ingrese numero de filas de la matriz 1: "); scanf("%d",&f);
+            printf("Ingrese numero de columnas de la matriz 1: "); scanf("%d",&c);
+            printf("Ingrese numero de filas de la matriz 2: "); scanf("%d",&f1);
+            printf("Ingrese numero de columnas de la matriz 2: "); scanf("%d",&c1);
+
+             matri2=(int **)malloc(sizeof(int *)*f);
+             resul=(int **)malloc(sizeof(int *)*f1);
+             g=(int **)malloc(sizeof(int *)*f);
+
+                for(int j=0;j<c;j++){
+                    matri2[j]=(int *)malloc(sizeof(int)*c); }
+
+                for(int j=0;j<c1;j++){
+                    resul[j]=(int *)malloc(sizeof(int)*c1);
+                    g[j]=(int *)malloc(sizeof(int)*c1); }
+
+                for(int i=0;i<f;i++){
+                    for(int j=0;j<c1;j++){
+                        g[i][j]=0; }
+                }
+
+            printf("\n*****MATRIZ 1*****\n\n");
+            dato(f,c,matri2);
+
+            printf("\n*****MATRIZ 2*****\n\n");
+            dato(f1,c1,resul);
+
+            mult (f,c,c1,matri2,resul,g);
+
+            printf("\n*****LA MULTIPLICACION ENTRE LAS DOS MATRICES ES: *****\n\n");
+            imprimir(f,c1,g);
+
+}
